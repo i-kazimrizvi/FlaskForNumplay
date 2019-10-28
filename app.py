3521a91680a76d8f99fd377630ccc5608b15a28f
Kazim Rizvi <i.kazimrizvi@gmail.com>
2019-10-28 20:07:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'name':'Hello World'}
+    
+    def post(self):
+        some_json = request.json
+        return {'sent': some_json}
+
+class Number2(Resource):
+    def get(self,num):
+        return {'calc':num*10}
+
+api.add_resource(HelloWorld,'/')
+api.add_resource(Number2,'/new/<int:num>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
