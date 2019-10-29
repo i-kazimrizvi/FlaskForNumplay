b27f56306856bbdf174c0522aaa7c2af67cd86a4
Kazim Rizvi <i.kazimrizvi@gmail.com>
2019-10-29 20:13:46 +0530

    new

--- app.py
+++ app.py
@@ -7,19 +7,17 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         return {'name':'Hello World'}
-    
+
     def post(self):
         some_json = request.json
         return {'sent': some_json}
 
 class Number2(Resource):
-    def get(self):
-        return {'calc':'100'}
+    def get(self, num):
+        return {'calc':num*15}
 
 api.add_resource(HelloWorld,'/')
-api.add_resource(Number2,'/new/')
+api.add_resource(Number2,'/new/<num>')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
