a7e39b8a45dac4deb031a148f1afb19fbdd03e62
Kazim Rizvi <i.kazimrizvi@gmail.com>
2019-10-29 20:27:47 +0530

    commit6

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('id')
 
 class HelloWorld(Resource):
     def get(self):
